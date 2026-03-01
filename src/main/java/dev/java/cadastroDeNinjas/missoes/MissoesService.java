fe53ce1f79ab827414f55a464df31b73c82da38c
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-01 19:57:25 -0300

    feat: adiciona metodo listarMissoes e a rota /listar

--- src/main/java/dev/java/cadastroDeNinjas/missoes/MissoesService.java
+++ src/main/java/dev/java/cadastroDeNinjas/missoes/MissoesService.java
@@ -1,5 +1,23 @@
 package dev.java.cadastroDeNinjas.missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    private  MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // listar todas minha missoes
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+
 
 }
