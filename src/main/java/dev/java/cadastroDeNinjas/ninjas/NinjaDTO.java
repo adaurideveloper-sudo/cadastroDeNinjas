a066749ebfcafc44deb1ebd250b77e9a4c055fb2
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-04 16:29:29 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidades x dto

--- src/main/java/dev/java/cadastroDeNinjas/ninjas/NinjaDTO.java
+++ src/main/java/dev/java/cadastroDeNinjas/ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.java.cadastroDeNinjas.ninjas;
 
+import dev.java.cadastroDeNinjas.missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private MissoesModel missao;
+    private String rank;
+
 }
