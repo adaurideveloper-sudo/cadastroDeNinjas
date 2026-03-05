8d2f24f691f345fae21c8a25443fd41f78898e32
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-05 10:55:27 -0300

    feat: Adiciona DTO e Mapper para missoes

---
+++ src/main/java/dev/java/cadastroDeNinjas/missoes/MissoesMapper.java
@@ -0,0 +1,29 @@
+package dev.java.cadastroDeNinjas.missoes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setNinjas(missoesDTO.getNinjas());
+
+        return missoesModel;
+
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setNinjas(missoesModel.getNinjas());
+
+        return missoesDTO;
+    }
+
+}
